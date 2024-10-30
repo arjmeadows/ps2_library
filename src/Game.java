5d176eb5963a4c0906b53f2f2a0d80ecc93dc6c3
adammeadows <arjmfreelance@gmail.com>
2024-10-30 22:56:00 +0000

    Added classes

---
+++ src/Game.java
@@ -0,0 +1,7 @@
+public class Game {
+
+    String title;
+    int year;
+    String developer;
+    String publisher;
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("PS2 Library");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setLayout(new FlowLayout());
+
+    }
+
+}
